db8110a5b435cf1f48ffdcec287baa1101f54935
Estefano-Solis-C <estefano.sebastian.solis.campos@gmail.com>
2026-08-28 00:31:43 -0500

    fix: resolve CS0118 User type-namespace collision in security tests

--- unit/backend/UserSecurityAndRoleTests.cs
+++ unit/backend/UserSecurityAndRoleTests.cs
@@ -3,9 +3,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using Moq;
 using Xunit;
+using UserAggregate = RentalPeAPI.User.Domain.Model.Aggregates.User;
 using RentalPeAPI.User.Application.Internal.CommandServices;
 using RentalPeAPI.User.Application.Internal.QueryServices;
-using RentalPeAPI.User.Domain.Model.Aggregates;
 using RentalPeAPI.User.Domain.Repositories;
 using RentalPeAPI.User.Domain.Services;
 using RentalPeAPI.Shared.Domain.Repositories;
@@ -31,7 +31,7 @@ namespace RentalPeAPI.Tests.Unit.Security
         public async Task Register_DuplicateEmail_ThrowsInvalidOperationException()
         {
             // Arrange
-            var existingUser = new User("Existing User", "existing@spacepulse.com", "hash", "+51999999999", "Homeowner", "");
+            var existingUser = new UserAggregate("Existing User", "existing@spacepulse.com", "hash", "+51999999999", "Homeowner", "");
             _mockUserRepo.Setup(r => r.FindByEmailAsync("existing@spacepulse.com")).ReturnsAsync(existingUser);
 
             var handler = new RentalPeAPI.User.Application.Internal.EventHandlers.RegisterUserCommandHandler(_mockUserRepo.Object, _mockHasher.Object, _mockUnitOfWork.Object);
@@ -46,7 +46,7 @@ namespace RentalPeAPI.Tests.Unit.Security
         public async Task Login_NonExistentUser_ThrowsException()
         {
             // Arrange
-            _mockUserRepo.Setup(r => r.FindByEmailAsync("unknown@spacepulse.com")).ReturnsAsync((User?)null);
+            _mockUserRepo.Setup(r => r.FindByEmailAsync("unknown@spacepulse.com")).ReturnsAsync((UserAggregate?)null);
             var handler = new LoginQueryHandler(_mockUserRepo.Object, _mockHasher.Object, _mockTokenGen.Object);
             var query = new LoginQuery("unknown@spacepulse.com", "Password123!");
 
@@ -60,7 +60,7 @@ namespace RentalPeAPI.Tests.Unit.Security
         public async Task Register_SupportedRoles_CreatesUserWithCorrectRole(string role)
         {
             // Arrange
-            _mockUserRepo.Setup(r => r.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync((User?)null);
+            _mockUserRepo.Setup(r => r.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync((UserAggregate?)null);
             _mockHasher.Setup(h => h.HashPassword(It.IsAny<string>())).Returns("secure_hash");
 
             var handler = new RentalPeAPI.User.Application.Internal.EventHandlers.RegisterUserCommandHandler(_mockUserRepo.Object, _mockHasher.Object, _mockUnitOfWork.Object);
